f5357b261180266fe29d5c61e19b095f122bfdd4
Ricky Ultimate <rickyogigi@gmail.com>
2024-11-21 13:06:47 +0100

    chore: remove import placeholders

--- app/server.go
+++ app/server.go
@@ -6,10 +6,6 @@ import (
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
