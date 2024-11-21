2721a14fd47886c6e6da49765a83f86eca54b57b
Ricky Ultimate <rickyogigi@gmail.com>
2024-11-21 13:17:41 +0100

    chore: Encapsulate connection request and response

--- app/server.go
+++ app/server.go
@@ -6,6 +6,18 @@ import (
 	"os"
 )
 
+func connect(conn net.Conn) {
+	buff := make([]byte, 1024)
+	_, err := conn.Read(buff)
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nConnction Established!\r\n"))
+	conn.Close()
+}
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -19,9 +31,7 @@ func main() {
 	}
 
 	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nConnction Established!\r\n"))
+
+	connect(conn)
+
 }
